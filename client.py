1fb7cebcae995161dd46d9f73bc4c411dd6c046f
BigCircle <zhuceafter2015@163.com>
2018-09-28 17:01:31 -0400

    after mesage sent, wait new input

--- client.py
+++ client.py
@@ -10,15 +10,19 @@ s.connect(('127.0.0.1', 65534)) # IP addr and port. For port, <1024 is for Inter
 d = s.recv(1024) # each time recive 1k bytes.
 print('Client receives:%s ' % d.decode('utf-8'))
    
-# send data
-usr_in = input('What do you want to send to server?:')
-d = bytes(usr_in, encoding = 'utf-8')
-s.sendall(d) # From python doc: Unlike send(), sendall() continues to send data from string until either all data has been sent or an error occurs.
-print('Client sends:%s ' % d.decode('utf-8'))
+while True:   
+    # send data
+    usr_in = input('What do you want to send to server?:')
+    if usr_in != "stop":
+        d = bytes(usr_in, encoding = 'utf-8')
+        s.sendall(d) 
+        print('Client sends:%s ' % d.decode('utf-8'))
 
-# receive reply
-d = s.recv(1024) # each time recive 1k bytes.  
-print('Client receives:%s ' % d.decode('utf-8'))
-
+        # receive reply
+        d = s.recv(1024) # each time recive 1k bytes.  
+        print('Client receives:%s ' % d.decode('utf-8'))
+    else:
+        break
+        
 # close socket
 s.close()
--- server.py
+++ server.py
@@ -13,16 +13,17 @@ print('Server is waiting for connection...')
 # process communication with clients
 def tcplink(sock, addr):
     print('Accept new connection form %s:%s...' % addr)
-    sock.send(b'Welcome client!')
+    wel = 'Welcome client' + (str(addr))
+    sock.send(bytes(wel, encoding = 'utf-8'))
     while True:
         data = sock.recv(1024)
         if not data:
             break
         data = data.decode('utf-8')
-        print('received: %s' % data)
+        print('received from ' + str(addr) + ':' + data)
         r_data = data[::-1].encode('utf-8') # [::-1] is used to reverse strings.
         sock.sendall(r_data)
-        print('reply: %s' % r_data.decode('utf-8'))
+        print('reply to ' + str(addr) + ':' + r_data.decode('utf-8'))
     sock.close()
     print('Connection form %s:%s closed.' % addr)
 
@@ -30,7 +31,7 @@ def tcplink(sock, addr):
 while True:
     # accept a new connection
     sock, addr = s.accept()
-    # create a thread for it
+    # create a thread for each client
     t = threading.Thread(target=tcplink, args=(sock, addr))
     t.start()
 
