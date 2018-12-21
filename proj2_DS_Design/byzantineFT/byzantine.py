e198df9c457991f25d6c6224c9651a72678c95d1
DayuanTan <zhuceafter2015@163.com>
2018-12-20 20:50:02 -0500

    byzantine

--- proj2_DS_Design/byzantineFT/byzantine.py
+++ proj2_DS_Design/byzantineFT/byzantine.py
@@ -4,8 +4,8 @@ import time
 import filecmp
 
 ip1="172.16.193.129"
-ip2="172.16.193.129"
-ip3="172.16.193.129"
+ip2="172.16.193.130"
+ip3="172.16.193.131"
 usrname="dayuan"
 pw="1111"
 
@@ -22,61 +22,43 @@ ip2filepath.strip()
 ip3filepath="/home/dayuan/forGithubRepo/proj2_DS_Design/byzantineFT/transactionLogip3.txt"
 ip3filepath.strip()
 
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-while(1==1):
-	time.sleep(2)
-
-	ssh.connect(hostname=ip1,port=22,username=usrname,password=pw)
+def checkandupdate(ip,thatipfilepath):
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh.connect(hostname=ip,port=22,username=usrname,password=pw)
 	stdid,stdout,stderr = ssh.exec_command('stat -c %Y ' + srcfilepath)
 	mtimeOfSrcFile = int(stdout.read().strip())
-	r = os.popen("stat -c %Y "+ip1filepath)
+	r = os.popen("stat -c %Y "+thatipfilepath)
 	mtimeOfLocalFile = r.read()
 	if int(mtimeOfSrcFile) > int(mtimeOfLocalFile):
 		transport = paramiko.Transport((ip1,22))
 		transport.connect(username=usrname,password=pw)
 		sftp = paramiko.SFTPClient.from_transport(transport)
-		sftp.get(srcfilepath,ip1filepath)
+		sftp.get(srcfilepath,thatipfilepath)
 		transport.close()
+	ssh.close()
 
+while(1==1):
+	time.sleep(2)
 
-	ssh.connect(hostname=ip2,port=22,username=usrname,password=pw)
-	stdid,stdout,stderr = ssh.exec_command('stat -c %Y ' + srcfilepath)
-	mtimeOfSrcFile = int(stdout.read().strip())
-	r = os.popen("stat -c %Y "+ip2filepath)
-	mtimeOfLocalFile = r.read()
-	if int(mtimeOfSrcFile) > int(mtimeOfLocalFile):
-		transport = paramiko.Transport((ip2,22))
-		transport.connect(username=usrname,password=pw)
-		sftp = paramiko.SFTPClient.from_transport(transport)
-		sftp.get(srcfilepath,ip2filepath)
-		transport.close()
+	checkandupdate(ip1,ip1filepath)
+	checkandupdate(ip2,ip2filepath)
+	checkandupdate(ip3,ip3filepath)
+	
 
-
-	ssh.connect(hostname=ip3,port=22,username=usrname,password=pw)
-	stdid,stdout,stderr = ssh.exec_command('stat -c %Y ' + srcfilepath)
-	mtimeOfSrcFile = int(stdout.read().strip())
-	r = os.popen("stat -c %Y "+ip3filepath)
-	mtimeOfLocalFile = r.read()
-	if int(mtimeOfSrcFile) > int(mtimeOfLocalFile):
-		transport = paramiko.Transport((ip3,22))
-		transport.connect(username=usrname,password=pw)
-		sftp = paramiko.SFTPClient.from_transport(transport)
-		sftp.get(srcfilepath,ip3filepath)
-		transport.close()
-
-
-
-	if ( filecmp.cmp(localfilepath,ip1filepath) and filecmp.cmp(localfilepath,ip2filepath) and ~filecmp.cmp(localfilepath,ip3filepath) ):
-		print("The file in "+ ip3 + "1 is not correct!!")
-	if ( filecmp.cmp(localfilepath,ip1filepath) and ~filecmp.cmp(localfilepath,ip2filepath) and filecmp.cmp(localfilepath,ip3filepath) ):
-		print("The file in "+ ip2 + "2 is not correct!!")
-	if ( ~filecmp.cmp(localfilepath,ip1filepath) and filecmp.cmp(localfilepath,ip2filepath) and filecmp.cmp(localfilepath,ip3filepath) ):
-		print("The file in "+ ip1 + "3 is not correct!!")
+	if ( filecmp.cmp(localfilepath,ip1filepath) and filecmp.cmp(localfilepath,ip2filepath) and not filecmp.cmp(localfilepath,ip3filepath) ):
+		filecmp.clear_cache()		
+		print("The file in "+ ip3 + " is not correct!!")
+	if ( filecmp.cmp(localfilepath,ip1filepath) and not filecmp.cmp(localfilepath,ip2filepath) and filecmp.cmp(localfilepath,ip3filepath) ):
+		filecmp.clear_cache()
+		print("The file in "+ ip2 + " is not correct!!")
+	if ( not filecmp.cmp(localfilepath,ip1filepath) and filecmp.cmp(localfilepath,ip2filepath) and filecmp.cmp(localfilepath,ip3filepath) ):
+		filecmp.clear_cache()
+		print("The file in "+ ip1 + " is not correct!!")
 	
 	
-	print("--")
+	if ( filecmp.cmp(localfilepath,ip1filepath) and filecmp.cmp(localfilepath,ip2filepath) and filecmp.cmp(localfilepath,ip3filepath) ):
+		filecmp.clear_cache()		
+		print("Everyone is same!")
 
 
